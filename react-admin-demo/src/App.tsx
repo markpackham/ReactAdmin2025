ae6a35335fe45888de1105934f89c54d0f16fec0
M <markpackham1@gmail.com>
2025-01-14 12:12:39 +0000

    Add UserShow
    https://youtu.be/PyaSnpXssks?si=VwBRLEc6taqBRlCR&t=1564

--- react-admin-demo/src/App.tsx
+++ react-admin-demo/src/App.tsx
@@ -1,16 +1,14 @@
-import {
-  Admin,
-  Resource,
-} from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import PostList from "./pages/posts/post-list.tsx";
 import UserList from "./pages/users/user-list.tsx";
 import PostShow from "./pages/posts/post-show.tsx";
+import UserShow from "./pages/users/user-show.tsx";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} show={PostShow} />
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} show={UserShow} />
   </Admin>
 );

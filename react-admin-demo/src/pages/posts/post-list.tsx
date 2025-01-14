48b6cc1563baea2fc573d2a4ced77d888ee3dde1
M <markpackham1@gmail.com>
2025-01-14 11:17:14 +0000

    Add spacing to header
    https://youtu.be/PyaSnpXssks?si=zCjk2tm6SFS9JgfD&t=1088

--- react-admin-demo/src/pages/posts/post-list.tsx
+++ react-admin-demo/src/pages/posts/post-list.tsx
@@ -1,14 +1,14 @@
-import { Datagrid, List, ReferenceField, TextField, SimpleList } from 'react-admin';
+import { Datagrid, List, ReferenceField, TextField, SimpleList, FunctionField } from 'react-admin';
 
 const PostList = () => (
   <List>
-    {/*<Datagrid>*/}
-    {/*  <TextField source="id" />*/}
-    {/*  <TextField source="title" />*/}
-    {/*  <TextField source="body" />*/}
-    {/*  <ReferenceField source="userId" reference="users" />*/}
-    {/*</Datagrid>*/}
-    <SimpleList primaryText={(record)=>record.title} secondaryText={(record)=>record.body}/>
+    <Datagrid sx={{'.RaDatagrid-headerCell':{padding:'16px'}}}>
+      <TextField source="id" />
+      <TextField source="title" label="Post Title" />
+      <FunctionField label="Excerpt" render={(record) => `${record.body.substring(0,50)}...`}/>
+      <ReferenceField source="userId" reference="users" />
+    </Datagrid>
+    {/*<SimpleList primaryText={(record)=>record.title} secondaryText={(record)=>record.body}/>*/}
   </List>
 );
 

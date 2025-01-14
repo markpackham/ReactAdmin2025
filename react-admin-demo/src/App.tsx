609019dbc2636f6f274a667307a7aba2b97f4ad7
M <markpackham1@gmail.com>
2025-01-14 12:09:13 +0000

    Replace default ShowGuesser with PostShow
    https://youtu.be/PyaSnpXssks?si=OGC5klni7dUSGo0y&t=1490

--- react-admin-demo/src/App.tsx
+++ react-admin-demo/src/App.tsx
@@ -6,10 +6,11 @@ import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import PostList from "./pages/posts/post-list.tsx";
 import UserList from "./pages/users/user-list.tsx";
+import PostShow from "./pages/posts/post-show.tsx";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} show={PostShow} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
